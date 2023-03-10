78e32d6bd5474f55ec8b9c5f3f73d6df29889c12
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-03-10 10:35:15 -0500

    first commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,28 +5,35 @@ export const NavigationBar = () => {
   return (
     <>
       <link rel="stylesheet" href="/static/css/NavigationBar.css"></link>
-      <div className="container">
-        <div className="navigation-bar row">
-            <div className="nav-item col s2">
-              <a title="Go to homepage">Home</a>
-            </div>
-            <div className="nav-item col s2">
-              <a title="Who I am, my hobbies and interests">About Me</a>
-            </div>
-            <div className="nav-item col s2">
-              <a title="Portfolio for ICS4UP course">ICS4UP</a>
-            </div>
-            <div className="nav-item col s2">
-              <a title="Current projects and links">Projects</a>
-            </div>
-            <div className="nav-item col s2">
-              <a title="Contact information and socials">Contact</a>
-            </div>
-            <div className="nav-item col s2">
-              <a title="Check out my personal side">Switch!</a>
-            </div>
+      {/* Dropdown structure */}
+      <ul id="dropdown1" class="dropdown-content">
+        <li><a title="Go to homepage">Home</a></li>
+        <li><a title="Who I am, my hobbies and interests">About Me</a></li>
+        <li><a title="Portfolio for ICS4UP course">ICS4UP</a></li>
+        <li><a title="Current projects and links">Projects</a></li>
+        <li><a title="Contact information and socials">Contact</a></li>
+        <li><a title="Check out my personal side">Switch!</a></li>
+      </ul>
+      {/* Navigation bar wrapper TODO: change color scheme to smth prettier */}
+      <nav className="teal">
+        <div className="nav-wrapper">
+          <a title="Go to homepage">
+            <img className="nav-icon" src="/static/img/profile_picture.png"></img>
+          </a>
+          <ul id="nav-mobile" className="left">
+            <li className="nav-item"><a title="Go to homepage">Home</a></li>
+            <li className="nav-item"><a title="Who I am, my hobbies and interests">About Me</a></li>
+            <li className="nav-item"><a title="Portfolio for ICS4UP course">ICS4UP</a></li>
+            <li className="nav-item"><a title="Current projects and links">Projects</a></li>
+            <li className="nav-item"><a title="Contact information and socials">Contact</a></li>
+            <li className="nav-item"><a title="Check out my personal side">Switch!</a></li>
+
+            {/* Normally hidden; only shows up when the size of the page gets small enough */}
+            <li><a class="dropdown-trigger" title="Navigation bar dropdown menu" data-target="dropdown1">Dropdown</a></li>
+          </ul>
         </div>
-      </div>
+      </nav>
+      <script src="/static/js/NavigationBar.js"></script>
     </>
   );
 }
